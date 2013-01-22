47d58cb5276785ebc5f0e9a346883cc41ee418f2
Claron <claront@gmail.com>
2013-01-21 17:13:37 -0700

    Finish 8.2.6 Signing out, also fixed a misplace 'end' that was causing failing tests.

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -18,7 +18,6 @@ describe "Authentication" do
         it { should_not have_selector('div.alert.alert-error') }
       end
     end
-  end
 
     describe "with valid information" do
       let(:user) { FactoryGirl.create(:user) }
@@ -33,5 +32,10 @@ describe "Authentication" do
       it { should have_link('Sign out', href: signout_path) }
       it { should_not have_link('Sign in', href: signin_path) }
 
+      describe "followed by signout" do
+        before { click_link "Sign out" }
+        it { should have_link('Sign in') }
+      end
+    end
   end
 end
